7c78035d32ffbb7be6d62036a3da8c229e08a71a
Ko Htutz <info@kohtut.me>
2022-03-25 03:46:11 +0630

    new : firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:stockcry/app_module.dart';
@@ -6,7 +7,7 @@ import 'package:stockcry/app_widget.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
  
-  // await Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(
     ModularApp(
       module: AppModule(),
